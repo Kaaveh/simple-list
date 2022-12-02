71fa815a6fbec5a533df8a15627265014e11c56d
kaaveh <kaaveh@pm.me>
2022-12-02 17:57:57 +0330

    Initial Commit

---
+++ Simple list/Simple_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_listApp.swift
+//  Simple list
+//
+//  Created by Kaaveh Mohamedi on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Simple_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
